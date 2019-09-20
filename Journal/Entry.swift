cbaa5de97f8b696a805bba7982583f0c9f782719
Josh Sparks <joshsparks@Joshs-MacBook-Pro.local>
2019-09-20 16:42:33 -0600

    Added data model

--- Journal/Entry.swift
+++ Journal/Entry.swift
@@ -8,26 +8,3 @@
 
 import Foundation
 
-class Entry: Equatable, Codable {
-	
-	init(timestamp: Date = Date(), title: String, text: String) {
-		
-		self.timestamp = timestamp
-		self.title = title
-		self.text = text
-	}
-	
-  // MARK: Properties
-	
-	var timestamp: Date
-	var title: String
-	var text: String
-}
-
-func ==(lhs: Entry, rhs: Entry) -> Bool {
-	if lhs.timestamp != rhs.timestamp { return false }
-	if lhs.title != rhs.title { return false }
-	if lhs.text != rhs.text { return false }
-	
-	return true
-}
